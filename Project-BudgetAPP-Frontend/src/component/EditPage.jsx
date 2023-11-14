a7a64774d5fd7c90a2482780c857ff204aaaa526
Christine Marchese <chm978@gmail.com>
2023-11-14 17:12:34 -0500

    new page and started edit page

--- Project-BudgetAPP-Frontend/src/component/EditPage.jsx
+++ Project-BudgetAPP-Frontend/src/component/EditPage.jsx
@@ -1,8 +1,23 @@
+import { useNavigate } from "react-router-dom";
 
 
- function EditPage() {
+function EditPage() {
+const navigate = useNavigate();
+const [date, seDate] = useState();
+
+
+
+function submitButton(e){
+  e.prevent.default();
+}
+
   return (
+   <>
     <div>EditPage</div>
+    <form>
+      <button onSubmit={submitButton}>Submit</button>
+    </form>
+  </>
   )
 }
 
--- Project-BudgetAPP-Frontend/src/component/NewPage.jsx
+++ Project-BudgetAPP-Frontend/src/component/NewPage.jsx
@@ -1,8 +1,67 @@
+  import { useNavigate } from "react-router-dom";
+  import { useState } from "react";
+  import axios from "axios";
 
 
  function NewPage() {
+ const navigate = useNavigate();
+ const API = import.meta.env.VITE_API_KEY;
+ const [date, setDate] = useState("");
+ const [name, setName] = useState();
+ const [amount, setAmount] = useState(0);
+ const [from, setFrom] = useState("");
+
+async function newButton(e) {
+   e.preventDefault();
+     try{
+       let result = await axios.post(`${API}/budget`,{
+         date: date,
+         name: name,
+         amount: amount,
+         from: from, 
+       });
+       navigate("/budget");
+       console.log(result);
+       return result;
+      }catch(error){
+      console.log(error);
+     }
+   }
   return (
-    <div>NewPage</div>
+   <>
+    <h2>Add a new item</h2>
+    <form onSubmit={newButton}>
+      <div>
+        <label>Date</label> 
+        <input type="text" id="date" name="date" value={date} onChange={(e) => 
+         setDate(e.target.value)} placeholder="date" required
+        />
+      </div>
+      <br/>
+      <div>
+        <label>Name</label>
+        <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}
+        placeholder="name" required
+        />
+      </div>
+      <br/>
+      <div>
+        <label>Amount</label>
+        <input type="number" id="amount" name="amount" value={amount} onChange={(e) => setAmount(e.target.value)}
+         placeholder="amount" required
+        />
+      </div>
+       <br/>
+       <div>
+        <label>From</label> 
+        <input type="text" id="from" name="from" value={from} onChange={(e) => setFrom(e.target.value)} 
+        placeholder="from" required
+        />
+        </div>
+        <br/>
+         <button>Create New Item</button>
+    </form> 
+   </>
   )
 }
 
